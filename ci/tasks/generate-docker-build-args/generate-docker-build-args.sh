c0d3b3928a9e9120d032e04bd5fc8e7351a53814
Daniel Lavoie <dlavoie@live.ca>
2019-05-19 17:56:41 -0400

    Fix Dockerfile name

--- ci/tasks/generate-docker-build-args/generate-docker-build-args.sh
+++ ci/tasks/generate-docker-build-args/generate-docker-build-args.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 PACK_VERISON=`cat pack-release/version`
 
-echo "{'PACK_VERSION': '${PACK_VERISON}'}" > docker-build-args/docker-build-args.json
+echo "{\"PACK_VERSION\": \"${PACK_VERISON}\"}" > docker-build-args/docker-build-args.json
